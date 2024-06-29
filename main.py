745b12302da4cc62b3c3f1316c82856e80a8c836
Calf79 <camerino_alfaro2@my.vcccd.edu>
2024-06-28 21:00:22 -0700

    Assignement-4-2-Camerino A.

--- main.py
+++ main.py
@@ -3,14 +3,22 @@ def main():
     a1 = int(input('Enter your first number: '))
     a2 = int(input('Enter your second number: '))
     N = int(input('Enter the number of sequences: '))
-    result.append(a1)
-    result.append(a2)
+   
     """
     ########################################
     Code Your Program here
     ########################################
     """
-
+    while True:
+        if N>2:
+            break
+        else:
+            print("N should be greater than 2")
+    result =[a1,a2]
+    for i in range(2,N):
+        num= result[-2] + result[-1]
+        result.append(num)
+        print(result)
     ########################################
     # Do not delete the return statement
     ########################################
